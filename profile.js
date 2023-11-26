6bd3768afae24ec226b6b923f471baa8ee5e7419
V Mahamanimaran <73939709+maran2611@users.noreply.github.com>
2023-11-26 23:56:48 +0530

    Delete profile.js

--- profile.js
+++
@@ -1,6 +0,0 @@
-function logoutUser() {
-    // Add your logout logic here
-    alert("Logout successful!");
-    // Redirect to the login page or perform other necessary actions
-    // window.location.href = 'login.html';
-}
