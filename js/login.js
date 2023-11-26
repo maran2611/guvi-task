1c03ec5f015571c39c250296785dfee60f1acf6a
V Mahamanimaran <73939709+maran2611@users.noreply.github.com>
2023-11-26 23:56:31 +0530

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,1 +1,31 @@
+function loginUser() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
+    // Create an object with user login data
+    const loginData = {
+        "username": username,
+        "password": password
+    };
+    console.log(username);
+
+
+
+
+   // Send a POST request to the PHP backe
+    fetch('http://localhost/guvi_task/php/login.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(loginData),
+        })
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Registration failed');
+        });
+ }
---
+++ js/profile.js
@@ -0,0 +1,6 @@
+function logoutUser() {
+    // Add your logout logic here
+    alert("Logout successful!");
+    // Redirect to the login page or perform other necessary actions
+    // window.location.href = 'login.html';
+}
