85f7daf508fe1fbf3d1b7a7c5b24c7c505a8d144
V Mahamanimaran <73939709+maran2611@users.noreply.github.com>
2023-11-27 21:10:47 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,44 +1,28 @@
-function registerUser() {
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
+$(document).ready(function() {
+    $("#submitBtn").click(function() {
+        var username = $("#name").val();
+        var email = $("#email").val();
+        var password = $("#password").val();
+        var confirmPassword = $("#confirmPassword").val();
 
-    // Check if passwords match
-    if (password !== confirmPassword) {
-        alert("Passwords do not match");
-        return;
-    }
-console.log(username);
-
-    // Create an object with user data
-    const userData = {
-        "username": username,
-        "email": email,
-        "password": password
-    };
-
-    // Send a POST request to the PHP backe
-    fetch('http://localhost/guvi_task/php/register.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
+        if (password != confirmPassword) {
+            alert("Password not match")
+        }
+        $.ajax({
+            type: "POST",
+            url: "php/register.php",
+            data: {
+                username: username,
+                email: email,
+                password: password
             },
-            body: JSON.stringify(userData),
-        })
-        .then(response => response.json())
-        .then(data => {
-            if(data.message == "Registration successful")
-            {
-                window.location.href = 'profile.html?username=' + username+'&email='+email;
-                return;
+            success: function(response) {
+                $("#result").html(response);
+            },
+            error: function(xhr, status, error) {
+                console.error("AJAX Error: " + status, error);
+                $("#result").html("An error occurred while processing your request.");
             }
-
-            alert(data.message);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Registration failed');
         });
-//        
-}
+    });
+});
