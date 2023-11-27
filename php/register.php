29cb804150d028dfdffb1c1c3aa03ce57600a7a9
V Mahamanimaran <73939709+maran2611@users.noreply.github.com>
2023-11-27 21:11:21 +0530

    Update register.php

--- php/register.php
+++ php/register.php
@@ -1,34 +1,32 @@
 <?php
-header('Content-Type: application/json');
+ $conn = new mysqli("localhost", "root", "", "registerinfo");
 
-// Assuming you have a MySQL database connection
-$servername = "localohost";
-$username = "root";
-$password = "";
 
-$conn = new mysqli("localhost","root","","kanna");
+ if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+ }
+if ($_POST['username']) {
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    $username = $_POST['username'];
+    
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $passw = password_hash($password, PASSWORD_DEFAULT);
+    $sql = $conn->query("INSERT INTO `reg`(`id`, `username`, `email`, `pass_word`) VALUES 
+    (NULL, '".$username."', '".$email."', '".$passw."')");
 
-// Assuming your HTML form fields are named username, email, and password
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+    
+    if ($sql === TRUE) {
+      echo "Registration Sucess";
+    } else {
+        echo "Error. Pls Check!.";   }
 
-// Hash the password before storing it in the database
-$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+   
+    $conn->close();
 
-$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashedPassword')";
-echo json_encode("hello users");
-if ($conn->query($sql) === TRUE) {
-    $response = array('message' => 'Registration successful');
-    echo json_encode($response);
+ 
 } else {
-    $response = array('message' => 'Registration failed');
-    echo json_encode($response);
+   
+    echo "Invalid request method.";
 }
-
-$conn->close();
 ?>
